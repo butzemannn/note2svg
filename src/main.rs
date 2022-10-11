8c24560f1388cc06e5ff71b4ea435d0434d3fc97
butzemannn <None>
2022-10-11 20:43:08 +0200

    implement archive extraction

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-    println!("Hello, world!");
+use std::path::Path;
+use std::process::Command;
+use clap::Parser;
+
+
+#[derive(Parser)]
+struct Cli {
+    file_path: String,
 }
+
+fn main() {
+    let args = Cli::parse();
+    let file = Path::new(&args.file_path);
+    extract_data(file);
+
+}
+fn extract_data(file: &Path) {
+    // Extracts note data into data/note/<Name>
+    let folder = &file.parent().unwrap();
+    let file = &file.to_str().unwrap();
+    let folder = folder.to_str().unwrap();
+
+    let mut result: String = folder.to_owned();
+    result.push_str("/note");
+
+    let output = Command::new("/usr/bin/unzip")
+        .args(["-o", &file, "-d", &result])
+        .output()
+        .expect("failed to unzip note");
+
+    dbg!(output);
+}
+
