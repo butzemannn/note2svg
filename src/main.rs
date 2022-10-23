20e17e1c8b74fdd8ba1727111853c621948d376b
butzemannn <None>
2022-10-24 00:46:32 +0200

    implement data unpacking and file loading

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use std::path::Path;
 use std::process::Command;
 use clap::Parser;
+use plist::{Dictionary, Value};
+use std::fmt::Error;
 
 
 #[derive(Parser)]
@@ -10,24 +12,49 @@ struct Cli {
 
 fn main() {
     let args = Cli::parse();
-    let file = Path::new(&args.file_path);
-    extract_data(file);
+    extract_data(args.file_path)
+        .expect("Could not extract data.");
 
 }
-fn extract_data(file: &Path) {
+
+fn extract_data(file_path: String) -> Option<()> {
     // Extracts note data into data/note/<Name>
-    let folder = &file.parent().unwrap();
-    let file = &file.to_str().unwrap();
-    let folder = folder.to_str().unwrap();
+    let file = Path::new(&file_path);
+    let folder = file.parent()?;
 
-    let mut result: String = folder.to_owned();
-    result.push_str("/note");
+    let file = file.to_str()?;
 
-    let output = Command::new("/usr/bin/unzip")
-        .args(["-o", &file, "-d", &result])
-        .output()
-        .expect("failed to unzip note");
+    // Append note subdirectory as extraction folder
+    let mut folder = folder.to_str()?.to_owned();
+    folder.push_str("/note");
 
-    dbg!(output);
+    // Extract file into folder
+    Command::new("/usr/bin/unzip")
+        .args(["-o", &file, "-d", &folder]);
+
+    Some(())
 }
+
+// Loads the session.plist file from the given file_path
+fn load_session(file_path: String) -> Result<Dictionary, Box<dyn std::error::Error>> {
+    let session = Value::from_file(file_path)?;
+
+    // Convert session to dict and return with ownership
+    let session = session
+        .as_dictionary()
+        .ok_or(Error)?
+        .to_owned();
+
+    Ok(session)
+}
+
+
+// steps:
+// 1. Iterate over all files in the notes directory
+// 2. Load Session.plist
+// 3. Read as dictionary and get curve points section
+// 4. Decode curve points with base64
+// 5. Convert curve points with float32 and little endian
+// 6. Write floats into list, 2 following floats can be interpreted as a 2D point
+// 7. Convert points into svg file format
 
